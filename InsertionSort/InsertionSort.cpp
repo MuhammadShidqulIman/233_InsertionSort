808c8b7e4d73eef465fadb6114d7f804ddc688d9
MuhammadShidqulIman <muhammadshidqul3@gmail.com>
2024-03-14 15:01:52 +0700

    add step 5

--- InsertionSort/InsertionSort.cpp
+++ InsertionSort/InsertionSort.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-void insertionsort() {
+void InsertionSort() {
 	int i, j, n;
 	int arr[20];
 
@@ -9,13 +9,20 @@ void insertionsort() {
 		temp = arr[i]; //step 2
 
 		j = i - 1; //step 3
-	}
-		
+
+		while (j >= 0 && arr[j]; > temp)
+		{
+
+			arr[j + 1] = arr[j] //step 4a
+			j--; //step 4b
+		}
+
+		arr[j + 1] = temp; //step 5
 		
 
 
 
 int main()
 {
-
+	std::cout <<
 }
