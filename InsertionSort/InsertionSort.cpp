8de51989e678a03d2b850134d26eaf760b91afbb
MuhammadShidqulIman <muhammadshidqul3@gmail.com>
2024-03-14 15:06:45 +0700

    Membuat variabel

--- InsertionSort/InsertionSort.cpp
+++ InsertionSort/InsertionSort.cpp
@@ -6,18 +6,7 @@ void InsertionSort() {
 	int arr[20];
 
 	for (i = 1; i <= n - 1; i++) { //step 1
-		temp = arr[i]; //step 2
-
-		j = i - 1; //step 3
-
-		while (j >= 0 && arr[j]; > temp)
-		{
-
-			arr[j + 1] = arr[j] //step 4a
-			j--; //step 4b
-		}
-
-		arr[j + 1] = temp; //step 5
+		
 		
 
 
